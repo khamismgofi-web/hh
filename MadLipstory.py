8e0b7c11bbdfd48ae7c13b8ed0d48a1ffea6a244
khamismgofi-web <khamismgofi18@gmail.com>
2026-02-07 17:04:24 +0300

    Add files via upload

---
+++ MadLipstory.py
@@ -0,0 +1,18 @@
+#this is the MAD LIP STORY project
+print("this is mad lib story")
+#to prompt user to subbmit the information 
+name=input("what is the your name: ")
+age=int(input("what is the your age")
+gender=input("what is the your gender: ")
+address
+=input("what is your home address: ")
+program=input("what is the your program and year of study: ")
+hobby=input("what is the your hobby: ")
+
+#this is the profile of user
+print("\n")
+print("----------this is the your profile----------")
+print("\n")
+print("my name is:", name +" i'm ", age )#name and age is on this line
+print("my gender is:", gender +" i come from' ", address )
+print("I study the program of :", program +" i like ", hobby )
